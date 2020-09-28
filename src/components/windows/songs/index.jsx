bb408090ceab014604b99131ac2d610af23fec5e
isaac Garcia <garciaisaac468@gmail.com>
2020-09-27 22:50:34 -0500

    the songs are ready to be played

--- src/components/windows/songs/index.jsx
+++ src/components/windows/songs/index.jsx
@@ -4,6 +4,22 @@ import './index.sass'
 
 const Songs = ({ songs }) => {
 
+  const [ metas, setMetas ] = useState([ ])
+  const [ listening, setListening ] = useState( '' )
+
+  const play = ( URI, meta ) => {
+    setListening( URI )
+    let player = document.getElementsByClassName( 'player' )[ 0 ]
+    console.log( Object.create( player ))
+    player.title = meta.title
+    player.ariaLabel = meta.title
+    let h = player.played
+    setTimeout(( ) => {
+      player.play( )
+      console.log( h )
+    }, 100)
+  }
+
   const toTimeFormat = allTime => {
     let hours, minutes, seconds, result = ''
     if( allTime > 3600 ) {
@@ -18,7 +34,7 @@ const Songs = ({ songs }) => {
     }
     seconds = Math.round( allTime )
     result = `${ result }${ seconds < 10 ? `0${ seconds }` : seconds }`
-    return result = `${ result } ${ hours !== undefined ? 'hr' : minutes !== undefined ? 'min' : 's' }`
+    return result = `${ result }${ hours !== undefined ? 'h' : minutes !== undefined ? 'm' : 's' }`
   }
 
   ( async () => {
@@ -43,28 +59,32 @@ const Songs = ({ songs }) => {
     }
   })( )
 
-  const [ metas, setMetas ] = useState([ ])
-
   return (
     <div className = 'songs'>
-      { metas !== undefined && metas.length > 0 && songs && songs.length > 0 ?
-        metas.map(( meta, i ) => (
-          <div key = { i } className = 'song'>
-            <img src = { meta.picture } className = 'cover' alt = { meta.album }/>
-            <div className = 'info'>
-              <span className = 'title'>{ meta.title }</span>
-              <span className = 'artists'>{
-                `${ meta.artists.length > 1 ?
-                  meta.artists.map( arst => arst )
-                  :
-                  meta.artists[ 0 ]}`
-              }</span>
-              <span className = 'time'>{ meta.duration }</span>
+      <div className = 'all'>
+        { metas !== undefined && metas.length > 0 && songs && songs.length > 0 ?
+          metas.map(( meta, i ) => (
+            <div
+              key = { i }
+              className = 'song'
+              onClick = { ( ) => play( songs[ i ].URI, meta ) }>
+              <img src = { meta.picture } className = 'cover' alt = { meta.album }/>
+              <div className = 'info'>
+                <span className = 'title'>{ meta.title }</span>
+                <span className = 'artists'>{
+                  `${ meta.artists.length > 1 ?
+                    meta.artists.map( arst => arst )
+                    :
+                    meta.artists[ 0 ]}`
+                }</span>
+                <span className = 'time'>{ meta.duration }</span>
+              </div>
             </div>
-          </div>
-        ))
-        :
-        null }
+          ))
+          :
+          null }
+        <audio src = { listening } className = 'player' controls />
+      </div>
     </div>
   )
 }
