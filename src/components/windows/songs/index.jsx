b1525eb94cd6fead7023a8b44bb0797578aa8a5d
isaac Garcia <garciaisaac468@gmail.com>
2020-10-20 00:24:31 -0500

    has been removed last details, primarily the 'data-setup component'

--- src/components/windows/songs/index.jsx
+++ src/components/windows/songs/index.jsx
@@ -1,43 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { ROUTES } from '../../../global/jsx'
 import './index.sass'
 
 import Card from '../../common/card'
 
-const Songs = ({ songs, window }) => {
-
-  const [ listening, setListening ] = useState( '' ) 
-
-  const play = ( URI, meta ) => {
-    setListening( URI )
-    let player = document.getElementsByClassName( 'player' )[ 0 ]
-    player.title = meta.title
-    player.ariaLabel = meta.title
-    setTimeout(( ) => {
-      player.play( )
-    }, 100)
-  }
-
-  return (
-    <div className = 'songs'>
-      <div className = 'all'>
-      { songs && songs.length > 0 ?
-          songs.map(( song, i ) => (
-            <Card
-              key = { i }
-              class = 'song'
-              img = { song.cover }
-              alt = { song.meta === undefined ? song.name : song.meta.album }
-              texts = { song.meta === undefined ? [ song.name, song.name, song.durationTimeFormat ] : [ song.meta.title, `${song.meta.artists.map( artist => artist )}`, song.durationTimeFormat ]}
-              action = {( ) => song.meta === undefined ? console.log( ) : play( song.URI, song.meta )}
-              tab = { window && window === ROUTES[ 1 ] ? '0' : '-1' }/>
-          ))
-          :
-          null }
-        <audio src = { listening } className = 'player' controls tabIndex = '-1' muted/>
-      </div>
+const Songs = ({ songs, window }) => (
+  <div className = 'songs'>
+    <div className = 'all'>
+    { songs && songs.length > 0 ?
+        songs.map(( song, i ) => (
+          <Card
+            key = { i }
+            class = 'song'
+            img = { song.cover }
+            alt = { song.meta === undefined ? song.name : song.meta.album }
+            texts = { song.meta === undefined ? [ song.name, song.name, song.durationTimeFormat ] : [ song.meta.title, `${song.meta.artists.map( artist => artist )}`, song.durationTimeFormat ]}
+            action = {( ) => console.log( song )}
+            tab = { window && window === ROUTES[ 1 ] ? '0' : '-1' }/>
+        ))
+        :
+        null }
     </div>
-  )
-}
+  </div>
+)
 
 export default Songs
